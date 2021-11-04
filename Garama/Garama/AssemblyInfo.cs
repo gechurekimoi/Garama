a357378ccbb2a41d8574c0b30e073d62ad2a2bee
v-jgechure <v-jgechure@DESKTOP-53S1MJR>
2021-11-05 01:57:52 +0300

    Add project files.

---
+++ Garama/Garama/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+
+[assembly: ExportFont("Riga-Black.ttf", Alias = "RigaBlack")]
+[assembly: ExportFont("Riga-Bold.ttf", Alias = "RigaBold")]
+[assembly: ExportFont("Riga-Medium.ttf", Alias = "RigaMedium")]
+[assembly: ExportFont("RigaScreen-Regular.ttf", Alias = "RigaScreenRegular")]
---
+++ Garama/Garama/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Garama.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Garama.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
