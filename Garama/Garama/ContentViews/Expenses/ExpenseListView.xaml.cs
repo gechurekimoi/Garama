8a2fa4328dfc23e2b187aea12ec197de2a0f8f9c
gechurekimoi <gechurejohn@gmail.com>
2021-11-12 02:13:07 +0300

    finished ui for expenses

--- Garama/Garama/ContentViews/Expenses/ExpenseListView.xaml.cs
+++ Garama/Garama/ContentViews/Expenses/ExpenseListView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Garama.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace Garama.ContentViews.Expenses
         public ExpenseListView()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushModalAsync(new AddExpensePage());
         }
     }
 }
