434175007180094b0441a060d7de0d25af84544e
gechurekimoi <gechurejohn@gmail.com>
2021-11-17 02:07:00 +0300

    done up to income list

---
+++ Garama/Garama/ContentViews/Income/IncomeList.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Garama.ContentViews.Income
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IncomeList : ContentView
+    {
+        public IncomeList()
+        {
+            InitializeComponent();
+        }
+    }
+}
