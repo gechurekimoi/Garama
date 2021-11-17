55530220bc35ad51b714e5db8b757f2a032f9cff
gechurekimoi <gechurejohn@gmail.com>
2021-11-18 02:07:16 +0300

    Added view to add income

--- Garama/Garama/ContentViews/Income/IncomeList.xaml.cs
+++ Garama/Garama/ContentViews/Income/IncomeList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Garama.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,18 @@ namespace Garama.ContentViews.Income
         public IncomeList()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                App.Current.MainPage.Navigation.PushModalAsync(new AddIncomePage());
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
